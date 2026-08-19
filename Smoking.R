65ce00d03f07af169a92aa0b09c43b8bf36be678
JSchmidt <scja234@gmail.com>
2026-08-19 11:00:04 +0200

    Imported dplyr, got Smoking Data for years d and e, filtered out two questions of interest, filtered out non-NA data,
    clean up

--- Smoking.R
+++ Smoking.R
@@ -1,23 +1,58 @@
 library(nhanesA)
+library(dplyr)
 
 #https://cran.r-project.org/web/packages/nhanesA/vignettes/Introducing_nhanesA.html 
 
 #two year surveys that start in uneven years: 2005 = 2006 survey
 
 
-#View(nhanesTables("EXAM", 2005)) # available variables in survey group
-#View(nhanesTableVars("EXAM", "BMX_D")) #show table variables
+View(nhanesTables("EXAM", 2005)) # available variables in survey group
+View(nhanesTableVars("EXAM", "BMX_D")) #show table variables
 length(nhanesSearchTableNames('BMX')) #list of tables that match pattern
 
 #get tables
 bmx_d <- nhanes("BMX_D")  # D = 2005-2006
 bmx_e <- nhanes("BMX_E")  # E = 2007-2008
 
+surveys = c("DEMO", "DIET", "EXAM", "LAB", "Q")
 
 
+#DEMO
+View(nhanesTables("DEMO", 2005))
 
-bmi_d <- bmx_d[,c(1,11)]
-bmi_e <- bmx_e[,c(1,11)]
+#EXAM
+View(nhanesTables("EXAM", 2005))
 
+#LAB
+View(nhanesTables("LAB", 2005))
+
+#Q
+View(nhanesTables("Q", 2005))
+View(nhanesTableVars("Q", "SMQ_D"))
+View(nhanes("SMQ_D"))
+
+# Smoking
+# check for years with available smoking data (A and K missing)
+smoking_years <- nhanesSearchTableNames('SMQ_') 
+
+#check if questions are identical 
+smq_d_vars <- nhanesTableVars("Q", "SMQ_D")$Variable.Name # Question Code in years d
+smq_e_vars <- nhanesTableVars("Q", "SMQ_E")$Variable.Name # Question Code in years e
+
+#filter out only the questions that are asked in both years
+common_vars <- nhanesTableVars("Q", "SMQ_D") |> 
+  filter(Variable.Name %in% intersect(smq_d_vars, smq_e_vars)) 
+
+# select question Code for questions of interest
+columns <- common_vars[c(1,4,5),1] # SEQN, age last smoked, amount smoked
+
+smq_d <- select(nhanes("SMQ_D"), all_of(columns)) # truncate data to questions of interest
+smq_e <- select(nhanes("SMQ_E"), all_of(columns))
+
+smq_d <- smq_d |> 
+  filter(!is.na(SMD055)) #filter out NA data (non smoker answers)
+
+smq_e <- smq_e |> 
+  filter(!is.na(SMD055))
 
 
