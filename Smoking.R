b7b644887a731e1537ea06c6a59adf2d1453ac84
JSchmidt <scja234@gmail.com>
2026-09-04 14:32:06 +0200

    Added notes for data selection, imported demographic and exam data

--- Smoking.R
+++ Smoking.R
@@ -17,21 +17,25 @@ bmx_e <- nhanes("BMX_E")  # E = 2007-2008
 surveys = c("DEMO", "DIET", "EXAM", "LAB", "Q")
 
 
-#DEMO
-View(nhanesTables("DEMO", 2005))
-
-#EXAM
-View(nhanesTables("EXAM", 2005))
+#### Data Selection Process ########
+# For each cohort (d, e)
+# 1. filter out metrics of interest: SEQN, bp, age, age last smoked, amount smoked
+# 2. filter out NAs
+# 3. merge lists (using SEQN) by intersecting strings (i.e. leaving out individuals with incomplete data) 
+# 
+#
+#################
 
 #LAB
-View(nhanesTables("LAB", 2005))
+#View(nhanesTables("LAB", 2005))
 
-#Q
+
+##### Smoking
+
 View(nhanesTables("Q", 2005))
 View(nhanesTableVars("Q", "SMQ_D"))
 View(nhanes("SMQ_D"))
 
-# Smoking
 # check for years with available smoking data (A and K missing)
 smoking_years <- nhanesSearchTableNames('SMQ_') 
 
@@ -54,5 +58,41 @@ smq_d <- smq_d |>
 
 smq_e <- smq_e |> 
   filter(!is.na(SMD055))
+
+# average age of last smoked in cohort d and e
+mean(smq_d$SMD055)
+mean(smq_e$SMD055)
+
+# average amount smoked
+mean(smq_d$SMD057)
+mean(smq_e$SMD057)
+
+####### DEMO
+View(nhanesTables("DEMO", 2005))
+View(nhanesTableVars("DEMO", "DEMO_D"))
+demo_d <- nhanes("DEMO_D") #get demographics for d & e cohort
+demo_e <- nhanes("DEMO_E")
+
+#select out SEQN and age (collumns: 38, 28)
+#View(nhanesTableVars("DEMO", "DEMO_D"))
+#View(nhanesTableVars("DEMO", "DEMO_E"))
+
+demo_d |> select(all_of(c("SEQN", "RIDAGEEX")))
+demo_e |> select(all_of(c("SEQN", "RIDAGEEX")))
+
+mean(demo_d$RIDAGEEX, na.rm = TRUE)/12 # average age
+mean(demo_e$RIDAGEEX, na.rm = TRUE)/12
+
+
+######EXAM
+View(nhanesTables("EXAM", 2005))
+View(nhanesTableVars("EXAM", "BPX_D"))
+bpx_d <- nhanes("BPX_D") #get bp data for d,e cohort
+bpx_e <- nhanes("BPX_E")
+
+
+
+
+
 
 
